094a5559ff55722ea1263062b07b294aff80546e
usrki <usrki@student.kit.edu>
2019-12-13 22:34:11 +0100

    update:face recognition in each person bounding box

--- long_distance_people_recognition_with_kalman_filter_final.py
+++ long_distance_people_recognition_with_kalman_filter_final.py
@@ -14,7 +14,7 @@ import json
 font = ImageFont.truetype('simhei.ttf',20,encoding='utf-8')
 cfgfile = "cfg/yolov3.cfg"
 weightsfile ="model_data/yolov3.weights"
-num_classes = 1
+num_classes = 80
 #parameters from people detection
 classes = load_classes('data/coco.names')
 
@@ -61,34 +61,6 @@ def sellect_person(output):
         if i[-1] == 0:
             result.append(i)
     return result
-
-def to_xy(outputs):
-    '''
-    把output的格式变为2X1X人物数的多维矩阵
-    x,y为框的中心点
-    '''
-    output_xy = []
-    for output in outputs:
-        x = 0.5*(output[1] + output[3])
-        y = 0.5*(output[2] + output[4])
-        output_xy.append([[x],[y]])
-    return output_xy
-
-def xy_to_normal(outputs,tracks):
-    '''
-    中心点位置更新过后，将其换回原来的数据形式
-    '''
-    output_normal = []
-    i = 0
-    for output in outputs:
-        x_l = int(tracks[i].prediction[0] - 0.5*(output[3]-output[1]))
-        y_l = int(tracks[i].prediction[1] - 0.5*(output[4]-output[2]))
-        x_r = int(tracks[i].prediction[0] + 0.5*(output[3]-output[1]))
-        y_r = int(tracks[i].prediction[1] + 0.5*(output[4]-output[2]))
-        id = tracks[i].track_id
-        output_normal.append([x_l,y_l,x_r,y_r,id])
-        i+=1
-    return output_normal
 
 def main():
 ##########################################################################################################
@@ -160,7 +132,7 @@ def main():
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output/test.avi',fourcc, 15.0, (640,480),True)
     '''
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('test_video/test.avi')
     
     detect_time = []
     recogn_time = []
@@ -183,75 +155,52 @@ def main():
             img = img.cuda()
 
         time_a = time.time()
-        if count_yolo %1 ==0:
-            output = model(Variable(img), CUDA)                         #适配后的图像放进yolo网络中，得到检测的结果
-            output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)         
+
+        output = model(Variable(img), CUDA)                         #适配后的图像放进yolo网络中，得到检测的结果
+        output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
 
 
-            if type(output) == int:
-                fps  = ( fps + (1./(time.time()-start)) ) / 2
-                print("fps= %f"%(fps))
-                cv2.imshow("frame", orig_im)
-                key = cv2.waitKey(1)
-                if key & 0xFF == ord('q'):
-                    break
-                continue
-        
-            output[:,1:5] = torch.clamp(output[:,1:5], 0.0, float(inp_dim))/inp_dim                #夹紧张量，限制在一个区间内
-        
-            #im_dim = im_dim.repeat(output.size(0), 1)
-            output[:,[1,3]] *= color_image.shape[1]
-            output[:,[2,4]] *= color_image.shape[0]
-            output = output.cpu().numpy()
-            #output = sellect_person(output)                                       #把标签不是人的output去掉，减少计算量
-            output = np.array(output)
+        if type(output) == int:
+            fps  = ( fps + (1./(time.time()-start)) ) / 2
+            print("fps= %f"%(fps))
+            cv2.imshow("frame", orig_im)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                break
+            continue
 
-            output_update = output
-        elif count_yolo %1 != 0:
-            output = output_update
+        output[:,1:5] = torch.clamp(output[:,1:5], 0.0, float(inp_dim))/inp_dim                #夹紧张量，限制在一个区间内
+
+        #im_dim = im_dim.repeat(output.size(0), 1)
+        output[:,[1,3]] *= color_image.shape[1]
+        output[:,[2,4]] *= color_image.shape[0]
+        output = output.cpu().numpy()
+        output = sellect_person(output)                                       #把标签不是人的output去掉，减少计算量
+        output = np.array(output)
+
         count_yolo += 1
-        #list(map(lambda x: write(x, orig_im), output))                #把结果加到原来的图像中
+        list(map(lambda x: write(x, orig_im), output))                #把结果加到原来的图像中
         #output的[0,1:4]分别为框的左上和右下的点的位置
         detect_time.append(time.time() - time_a)
 ###########################################################################################################
-        #kalman filter tracking part
+        # face recognition part
         time_a = time.time()
-        output_kalman_xywh = to_xy(output)                   #把output数据变成适合kalman更新的类型
-        if (len(output_kalman_xywh) > 0):
-            tracker.Update(output_kalman_xywh)                #用kalman filter更新框的位置
-        
-        outputs_kalman_normal = np.array(xy_to_normal(output,tracker.tracks)) #换回原来的数据形式
-        kalman_time.append(time.time() - time_a)
-
-
-        time_a = time.time()
-        for output_kalman_normal in outputs_kalman_normal:
+        for person_bbox in output:
             #draw the bounding box
-            left,top,right,down = [int(max(0,x)) for x in output_kalman_normal[0:4]]
-            cv2.rectangle(orig_im, (left,top), (right,down),(255,255,255), 2)
-            cv2.putText(orig_im, str(output_kalman_normal[4]),(left,top),0, 5e-3 * 200, (0,255,0),2)              #track id 就是数字
+            top,left,down,right = [int(x) for x in person_bbox[1:5]]
+            if left>=right or top >= down:
+                continue
+            person_img = color_image[left:right,top:down].copy()
 
-########################################################################################################
-        #face recognition part
-            person_img = orig_im[left:min(right,640),top:min(down,480)].copy()
-            print(left,right,top,down)
-            print(person_img.shape)
             img = Image.fromarray(person_img)
             bboxes, landmark = detect_faces(img)                                                                  #首先检测脸
 
             if len(bboxes) == 0:
-                print('detect no people')
+                print('detect no face')
             else:
-                print('the length  ', len(bboxes))
+                print('detect face!!!!!')
                 for bbox in bboxes:
-                    print(bbox)
-
                     loc_x_y = [bbox[2]+top, bbox[1]+left]
-                    '''
-                    cv2.imshow('person',person_img)
-                    if cv2.waitKey(0) or 0xFF == ord('q'):
-                        cv2.destroyAllWindows()
-                    '''
                     face_img = person_img[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])].copy()              #从图像中截取框
                     feature = np.squeeze(get_feature(face_img, model_facenet, trans, device))                               #框里的图像计算feature
                     cos_distance = cosin_metric(total_features, feature)
